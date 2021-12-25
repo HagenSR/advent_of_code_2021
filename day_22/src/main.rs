32098ffdd7424bc728e21baa299aa0733de7aaef
sean <sean.r.hagen@gmail.com>
2021-12-25 12:15:31 -0600

    WIP day 22

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, collections::BTreeMap};
+use std::{fs};
 
 struct Instruction {
     state: bool,
@@ -10,8 +10,36 @@ struct Instruction {
     z_end: i32,
 }
 
+impl Clone for Instruction {
+    fn clone(&self) -> Self {
+        Self {
+            state: self.state.clone(),
+            x_start: self.x_start.clone(),
+            x_end: self.x_end.clone(),
+            y_start: self.y_start.clone(),
+            y_end: self.y_end.clone(),
+            z_start: self.z_start.clone(),
+            z_end: self.z_end.clone(),
+        }
+    }
+}
+
+impl PartialEq for Instruction {
+    fn eq(&self, other: &Self) -> bool {
+        return self.state == other.state
+            && self.x_start == other.x_start
+            && self.x_end == other.x_end
+            && self.y_start == other.y_start
+            && self.y_end == other.y_end
+            && self.z_start == other.z_start
+            && self.z_end == other.z_end;
+    }
+}
+
+impl Eq for Instruction {}
+
 fn main() {
-    let filename = "src/data/data.txt";
+    let filename = "src/data/datasmall.txt";
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let split_new_line: Vec<&str> = contents.split("\n").collect();
     let mut instructions: Vec<Instruction> = Vec::new();
@@ -21,7 +49,7 @@ fn main() {
         if split_space[0] == "on" {
             on_or_off = true;
         }
-        let mut dimensions: Vec<Vec<i32>> = split_space[1]
+        let dimensions: Vec<Vec<i32>> = split_space[1]
             .replace("x", "")
             .replace("=", "")
             .replace("y", "")
@@ -30,19 +58,6 @@ fn main() {
             .map(|res| res.split("..").map(|f| f.parse::<i32>().unwrap()).collect())
             .collect();
 
-        for vec in dimensions.iter_mut(){
-            if vec[0] < -50{
-                vec[0] = -50;
-            }else if vec[0] > 50{
-                vec[0] = 50;
-            }
-
-            if vec[1] < -50{
-                vec[1] = -51;
-            }else if vec[1] > 50{
-                vec[1] = 49;
-            }
-        }
         let instruct: Instruction = Instruction {
             state: on_or_off,
             x_start: dimensions[0][0],
@@ -58,31 +73,68 @@ fn main() {
     first(instructions);
 }
 
-fn first(instructions : Vec<Instruction>){
-    let mut flipped : BTreeMap<(i32, i32, i32), bool> = BTreeMap::new();
-    let mut counter = 0;
-    for instruct in instructions{
-        for x in instruct.x_start..instruct.x_end{
-            for y in instruct.y_start..instruct.y_end{
-                for z in instruct.z_start..instruct.z_end{
-
-                    let value = flipped.entry((x,y,z)).or_insert(instruct.state);
-                    *value = instruct.state;
+fn first(instructions: Vec<Instruction>) {
+    let mut previously_calculated_instructions: Vec<Instruction> = Vec::new();
+    for instruct in instructions.iter() {
+        let mut new_intersections: Vec<Instruction> = previously_calculated_instructions.clone();
+        for prev_instruct in previously_calculated_instructions.iter_mut() {
+            if determine_colision(prev_instruct, instruct) && (prev_instruct.state != false && instruct.state != false) {
+                let overlap = return_overlap(instruct, &prev_instruct);
+                if !new_intersections.contains(&overlap) {
+                    new_intersections.push(overlap.clone());
                 }
             }
         }
-        counter+=1;
-        println!("Instruct {}", counter);
-    }
-    println!("There were {} on values", count_on_values(flipped));
-}
-
-fn count_on_values(flipped : BTreeMap<(i32, i32, i32), bool>) -> i32 {
-    let mut rtn = 0;
-    for value in flipped.values(){
-        if *value{
-            rtn += 1;
+        previously_calculated_instructions = new_intersections;
+        if instruct.state {
+            previously_calculated_instructions.push(instruct.clone());
         }
     }
-    return  rtn;
+    println!(
+        "There were {} on values",
+        determine_final_area(previously_calculated_instructions)
+    );
+}
+
+fn determine_colision(a: &Instruction, b: &Instruction) -> bool {
+    return (a.x_start <= b.x_end && a.x_end >= b.x_start)
+        && (a.y_start <= b.y_end && a.y_end >= b.y_start)
+        && (a.z_start <= b.z_end && a.z_end >= b.z_start);
+}
+
+fn determine_final_area(list: Vec<Instruction>) -> i32 {
+    let mut area = 0;
+    for instruction in list {
+        let tmp = calculate_area(&instruction);
+        if instruction.state {
+            area += tmp;
+        } else {
+            area -= tmp;
+        }
+    }
+    return area;
+}
+
+fn return_overlap(a: &Instruction, b: &Instruction) -> Instruction {
+    let x_start = a.x_start.max(b.x_start);
+    let x_end = a.x_end.min(b.x_end);
+    let y_start = a.y_start.max(b.y_start);
+    let y_end = a.y_end.min(b.y_end) ;
+    let z_start = a.z_start.max(b.z_start);
+    let z_end = a.z_end.min(b.z_end);
+    return Instruction {
+        state: !(a.state && b.state),
+        x_start,
+        x_end,
+        y_start,
+        y_end,
+        z_start,
+        z_end,
+    };
+}
+
+fn calculate_area(instruct: &Instruction) -> i32 {
+    return (instruct.x_end - instruct.x_start)
+        * (instruct.z_end - instruct.z_start)
+        * (instruct.y_end - instruct.y_start);
 }
